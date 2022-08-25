a0cd493a74d39c6b3d19a5f8916200f08b1b07d4
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-25 07:59:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '3Temas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
