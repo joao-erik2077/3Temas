7cab15d264909a57c3743dd37a6e1e7959dc03b2
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-25 08:33:28 -0300

    Added tab 1 design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public linguagens: any[] = [
+    {
+      nome: 'Javascript',
+      img: 'https://logospng.org/download/javascript/logo-javascript-256.png'
+    },
+    {
+      nome: 'Typescript',
+      img: 'https://cdn.iconscout.com/icon/free/png-256/typescript-1174965.png'
+    },
+    {
+      nome: 'Java',
+      img: 'https://cdn-icons-png.flaticon.com/512/226/226777.png'
+    },
+    {
+      nome: 'Python',
+      img: 'https://miro.medium.com/max/672/1*ztqS5rRI29GHxZa6uPF2UA.png'
+    },
+    {
+      nome: 'C++',
+      img: 'https://logospng.org/download/c-plus-plus/c-plus-plus-256.png'
+    },
+  ];
+
   constructor() {}
 
 }
