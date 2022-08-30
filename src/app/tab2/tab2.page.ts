4be2d353f17510f81c9f79b2aee47e19dfa96233
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-08-30 14:56:18 -0300

    tab 2 base design

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,122 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public readonly cant = '#ff4b2b';
+  public readonly can = '#25b4ec';
+  public readonly take = '#e9ff27';
+  public readonly all = '#4eff2b';
+
+  public circleColors = [
+    this.cant, // AB+
+    this.cant, // A+
+    this.cant, // B+
+    this.cant, // O+
+    this.cant, // AB-
+    this.cant, // A-
+    this.cant, // B-
+    this.cant, // O-
+  ];
+
   constructor() {}
 
+  changeBloodType(event) {
+    switch (event) {
+      case 'AB+':
+        this.circleColors = [
+          this.all, // AB+
+          this.take, // A+
+          this.take, // B+
+          this.take, // O+
+          this.take, // AB-
+          this.take, // A-
+          this.take, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'AB-':
+        this.circleColors = [
+          this.can, // AB+
+          this.cant, // A+
+          this.cant, // B+
+          this.cant, // O+
+          this.all, // AB-
+          this.take, // A-
+          this.take, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'A+':
+        this.circleColors = [
+          this.can, // AB+
+          this.all, // A+
+          this.cant, // B+
+          this.take, // O+
+          this.cant, // AB-
+          this.take, // A-
+          this.cant, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'A-':
+        this.circleColors = [
+          this.can, // AB+
+          this.can, // A+
+          this.cant, // B+
+          this.cant, // O+
+          this.can, // AB-
+          this.all, // A-
+          this.cant, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'B+':
+        this.circleColors = [
+          this.can, // AB+
+          this.cant, // A+
+          this.all, // B+
+          this.take, // O+
+          this.cant, // AB-
+          this.cant, // A-
+          this.take, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'B-':
+        this.circleColors = [
+          this.can, // AB+
+          this.cant, // A+
+          this.can, // B+
+          this.cant, // O+
+          this.can, // AB-
+          this.cant, // A-
+          this.all, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'O+':
+        this.circleColors = [
+          this.can, // AB+
+          this.can, // A+
+          this.can, // B+
+          this.all, // O+
+          this.cant, // AB-
+          this.cant, // A-
+          this.cant, // B-
+          this.take, // O-
+        ];
+        break;
+      case 'O-':
+        this.circleColors = [
+          this.can, // AB+
+          this.can, // A+
+          this.can, // B+
+          this.can, // O+
+          this.can, // AB-
+          this.can, // A-
+          this.can, // B-
+          this.all, // O-
+        ];
+        break;
+    }
+  }
 }
