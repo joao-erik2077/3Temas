288beb291b92cc421d92233b758c38e720a81054
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-08-30 16:03:22 -0300

    linguagem page base design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,43 +13,78 @@ export class Tab1Page {
       nome: 'Javascript',
       img: 'https://logospng.org/download/javascript/logo-javascript-256.png',
       books: [
-        'https://github.com/braziljs/eloquente-javascript',
-        'http://jstherightway.org/pt-br/',
-        'http://stack.desenvolvedor.expert/comecando/quando-usar.html#quando-usar-um-devstack-javascript',
+        {
+          nome: 'Eloquente Javascript',
+          link: 'https://github.com/braziljs/eloquente-javascript',
+        },
+        {
+          nome: 'Javascript do jeito certo',
+          link: 'http://jstherightway.org/pt-br/',
+        },
+        {
+          nome: 'Quando usar um STACK Javascript',
+          link: 'http://stack.desenvolvedor.expert/comecando/quando-usar.html#quando-usar-um-devstack-javascript',
+        },
       ],
     },
     {
       nome: 'Typescript',
       img: 'https://cdn.iconscout.com/icon/free/png-256/typescript-1174965.png',
       books: [
-        'https://oieduardorabelo.medium.com/typescript-o-guia-definitivo-1a63b04259cc',
-        'https://www.maiconsilva.com/starting-typescript/',
+        {
+          nome: 'Typescript o guia definitivo',
+          link: 'https://oieduardorabelo.medium.com/typescript-o-guia-definitivo-1a63b04259cc',
+        },
+        {
+          nome: 'Começando Typescript',
+          link: 'https://www.maiconsilva.com/starting-typescript/',
+        },
       ],
     },
     {
       nome: 'Java',
       img: 'https://cdn-icons-png.flaticon.com/512/226/226777.png',
       books: [
-        'http://professor.unisinos.br/pjaques/material/java_basico.pdf',
-        'https://www.alura.com.br/apostila-java-web/',
-        'https://www.alura.com.br/apostila-java-orientacao-objetos/',
+        {
+          nome: 'Java básico',
+          link: 'http://professor.unisinos.br/pjaques/material/java_basico.pdf',
+        },
+        {
+          nome: 'Apostila Java Web',
+          link: 'https://www.alura.com.br/apostila-java-web/',
+        },
+        {
+          nome: 'Apostila Java POO',
+          link: 'https://www.alura.com.br/apostila-java-orientacao-objetos/',
+        },
       ],
     },
     {
       nome: 'Python',
       img: 'https://miro.medium.com/max/672/1*ztqS5rRI29GHxZa6uPF2UA.png',
       books: [
-        'https://aprendendo-computacao-com-python.readthedocs.io/en/latest/index.html',
-        'https://penseallen.github.io/PensePython2e/',
-        'https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyYWZhZWx1ZnR8Z3g6NTJlM2UzYzY1ZTgzMDEwMw',
+        {
+          nome: 'Aprendendo computação com Python',
+          link: 'https://aprendendo-computacao-com-python.readthedocs.io/en/latest/index.html',
+        },
+        {
+          nome: 'Pense em Python',
+          link: 'https://penseallen.github.io/PensePython2e/',
+        },
+        {
+          nome: 'Python orientado a objetos',
+          link: 'https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyYWZhZWx1ZnR8Z3g6NTJlM2UzYzY1ZTgzMDEwMw',
+        },
       ],
     },
     {
       nome: 'C++',
       img: 'https://logospng.org/download/c-plus-plus/c-plus-plus-256.png',
       books: [
-        'https://www.ime.usp.br/~slago/slago-C++.pdf',
-        'http://calhau.dca.fee.unicamp.br/wiki/images/0/01/EstruturasDados.pdf'
+        {
+          nome: 'Linguagem C++',
+          link: 'https://www.ime.usp.br/~slago/slago-C++.pdf',
+        },
       ],
     },
   ];
